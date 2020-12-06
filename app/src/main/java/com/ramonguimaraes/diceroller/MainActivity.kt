e007593e6e9eed73874ae407b5c5fd62b40893b9
Ramon de Guimarães Apolinario <r4mon.guimaraes@gmail.com>
2020-12-05 21:06:40 -0300

    Refactoring Code

--- app/src/main/java/com/ramonguimaraes/diceroller/MainActivity.kt
+++ app/src/main/java/com/ramonguimaraes/diceroller/MainActivity.kt
@@ -13,10 +13,13 @@ import java.lang.NumberFormatException
 class MainActivity : AppCompatActivity() {
 
     var diceFace: Int = 1
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         val countUp: Button = findViewById(R.id.count_up)
@@ -38,34 +41,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun setDiceImage() {
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (diceFace) {
 
-        when (diceFace) {
-            1 -> {
-                diceImage.setImageResource(R.drawable.dice_1)
-                diceImage.contentDescription = "1"
-            }
-            2 -> {
-                diceImage.setImageResource(R.drawable.dice_2)
-                diceImage.contentDescription = "2"
-            }
-            3 -> {
-                diceImage.setImageResource(R.drawable.dice_3)
-                diceImage.contentDescription = "3"
-            }
-            4 -> {
-                diceImage.setImageResource(R.drawable.dice_4)
-                diceImage.contentDescription = "4"
-            }
-            5 -> {
-                diceImage.setImageResource(R.drawable.dice_5)
-                diceImage.contentDescription = "5"
-            }
-            6 -> {
-                diceImage.setImageResource(R.drawable.dice_6)
-                diceImage.contentDescription = "6"
-            }
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
         }
+
+        imageView.setImageResource(drawableResource)
 
     }
 
